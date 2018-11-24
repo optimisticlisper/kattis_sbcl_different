5a99ea1233e55a5f95721a8b8088f7398d33e4d1
macaroni destroyer <code.miles.code@gmail.com>
2018-11-24 09:30:42 -0900

    modified accepted.lisp to format output correctly

--- accepted.lisp
+++ accepted.lisp
@@ -1,1 +1,2 @@
-(loop for first = (read *standard-input* nil nil) until (null first) do (print (abs (- first (read)))))
+(loop for first = (read *standard-input* nil nil) until (null first)
+   do (prin1 (abs (- first (read)))) (terpri))
